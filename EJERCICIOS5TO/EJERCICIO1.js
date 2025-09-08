eabecedd9b6b862ce05e9b86bdfcd0400761a57d
merryrdgz <23308051220758@cbtis122.edu.mx>
2025-09-07 20:31:39 -0600

    Add files via upload

---
+++ EJERCICIOS5TO/EJERCICIO1.js
@@ -0,0 +1,11 @@
+function displayMessage() {
+    var message = "Hello, World!";
+    if (true) {
+      let message = "Hello, JavaScript!";
+      console.log(message); // Imprime "Hello, JavaScript!" debido al alcance de bloque de `let`.
+    }
+    console.log(message); // Imprime "Hello, World!" debido al alcance de función de `var`.
+  }
+  
+  // Llama a la función
+  displayMessage();
